2621a0fa75be8a36d4e2f5ca45c00d8dd40b9340
shrey kilaji <kilajishrey123@gmail.com>
2025-06-27 20:30:40 +0530

    Method Overloading concept exercise

---
+++ src/com/testpackage/MethodOverloadingConcept.java
@@ -0,0 +1,24 @@
+package com.testpackage;
+
+//created class with same name methods with different number and types of parameters
+
+class Calculator{
+
+    int add(int a, int b){
+        return a+b;
+    }
+    double add(double a,double b, double c){
+        return a+b+c;
+    }
+}
+
+public class MethodOverloadingConcept{
+    public static void main(String[] args){
+
+        Calculator calc= new Calculator();
+
+        //same name methods 'add()' with different number and types of parameters
+        System.out.println("Total of int: "+ calc.add(5,10));  //add() method with two parameter a & b.
+        System.out.println("Total of double: "+ calc.add(12,13,14)); //add() method with three parameter a , b & c.
+    }
+}
