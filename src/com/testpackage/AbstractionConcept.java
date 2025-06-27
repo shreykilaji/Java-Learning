fcb0204c0d676b87df609c3123db9584c80d3562
shrey kilaji <kilajishrey123@gmail.com>
2025-06-27 18:25:51 +0530

    Abstraction Concept Exercise

---
+++ src/com/testpackage/AbstractionConcept.java
@@ -0,0 +1,39 @@
+package com.testpackage;
+
+// Abstract class representing a vehicle (hiding implementation details)
+abstract class Vehicle {
+        //abstract method (what it can do)
+        abstract void accelerate();
+        abstract void brake();
+
+        // concrete method (common to all vehicle)
+        void startEngine(){
+            System.out.println("Engine started!");
+        }
+    }
+//Concrete Implementation (hidden details)
+/*creating a Car class which have implementation of accelerate() and brake() method
+which is overridden here in this class.
+*/
+
+class Car extends Vehicle{
+    void accelerate(){
+        System.out.println("Car: Pressing gas pedal..");
+        //hidden complex logic behind acceleration : fuel injection, gearshift etc.
+    }
+    void brake(){
+        System.out.println("Car: Applying brakes");
+        //hidden logic behind brake system
+    }
+}
+
+// Implementing 'AbstractionConcept' class with main method to call methods by creating object of Vehicle class
+
+public class AbstractionConcept{
+    public static void main(String[] args){
+        Vehicle myCar= new Car();
+        myCar.startEngine();
+        myCar.accelerate();
+        myCar.brake();
+    }
+}
