42ba5719543c00a3a0ef85954752ab5c73dfc760
shrey kilaji <kilajishrey123@gmail.com>
2025-06-28 16:35:50 +0530

    File handling Java (Read content of file)

---
+++ src/com/testpackage/FileHandlingReadFile.java
@@ -0,0 +1,30 @@
+package com.testpackage;
+/* We need 'Scanner' class to read data from a file. Also need 'File' class from where we get the file details.
+from where data is to be read.
+
+first create obj of 'File' class and pass file name in it.
+then create obj of 'Scanner' class and pass the obj of 'File' class in it to access file for reading.
+ */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileHandlingReadFile {
+    public static void main(String[] args){
+
+        try {
+            File myNewObj = new File("E:\\JavaFileHandlingFolder\\TestFile1.txt");
+            Scanner myReader = new Scanner(myNewObj);
+
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        }
+        catch(Exception e){
+         System.out.println("Error occurred.");
+         e.printStackTrace();
+        }
+    }
+}
