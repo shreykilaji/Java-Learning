be30a694aba3965fc2909abbc102555ded8bec33
shrey kilaji <kilajishrey123@gmail.com>
2025-06-28 16:00:29 +0530

    File Handling java (Write in file).

---
+++ src/com/testpackage/WriteToFile.java
@@ -0,0 +1,25 @@
+/*To Write in File in Java need to import 'FileWriter' Class from 'java.io' package
+ Then we create Obj of FileWriter Class & use write() method to write in File.
+ after successfully writing we need to close the file using close() method to close file.
+*/
+package com.testpackage;
+
+import java.io.FileWriter; //import 'FileWriter' class from 'java.io' package.
+import java.io.IOException; //Import 'IOException' class from 'java.io' package to handle error.
+
+public class WriteToFile {
+    public static void main (String[] args){
+
+        try{
+            //Creating object of FileWriter Class
+            FileWriter myWriter = new FileWriter("E:\\JavaFileHandlingFolder\\TestFile1.txt");
+            myWriter.write("Hello I am Writing this from File Handling Code.");
+            myWriter.close();
+            System.out.println("Successfully writing completed in File: ");
+        }
+        catch(Exception e){
+            System.out.println("Error Occurred");
+            e.printStackTrace();
+        }
+    }
+}
