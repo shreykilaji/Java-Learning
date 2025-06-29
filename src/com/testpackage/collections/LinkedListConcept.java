967ffea22bcdd8ea7d94fe58cbcbdc3c1f500034
shrey kilaji <kilajishrey123@gmail.com>
2025-06-29 15:53:58 +0530

    LinkedListConcept-> offer(),poll() & peek() methods

---
+++ src/com/testpackage/collections/LinkedListConcept.java
@@ -0,0 +1,33 @@
+package com.testpackage.collections;
+
+/*We can Implement Queue Interface using LinkedList Class
+ import Queue in code
+ import LinkedList class-> this class has three main methods to be used
+ offer() ->to add element in queue
+ poll() -> Delete/ remove element from queue
+ peek() -> returns which next element to be deleted/removed from queue.
+ */
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class LinkedListConcept {
+    public static void main(String[] args) {
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        // Adding element in Queue-> using offer() method of linkedList class
+        queue.offer(20);
+        queue.offer(30);
+        queue.offer(40);
+
+        System.out.println(queue);
+
+        //Returns Delete/remove element from Queue-> using poll() method
+        System.out.println(queue.poll());
+
+        System.out.println(queue);
+
+        //Returns which next element to be removed/deleted-> using peek() method
+        System.out.println(queue.peek());
+    }
+}
